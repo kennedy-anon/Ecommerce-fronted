984b13f5f865c7689ee3cf1c211a962dad51be57
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2022-06-13 15:47:14 +0300

    Finished the add to cart functionality

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -12,7 +12,7 @@ export class CartService {
   constructor() { }
   getProducts(){
     return this.productList.asObservable();
-  }
+  } 
 
   setProduct(product: any){
     this.cartItemList.push(...product);
@@ -21,6 +21,30 @@ export class CartService {
 
   addToCart(product: any){
     this.cartItemList.push(product);
+    this.productList.next(this.cartItemList);
+    this.getTotalPrice();
+    console.log(this.cartItemList);
+  }
+
+  getTotalPrice() : number{
+    let grandTotal = 0;
+    this.cartItemList.map((a:any)=>{
+      grandTotal += a.total;
+    })
+    return grandTotal;
+  }
+
+  removeCartItem(product: any){
+    this.cartItemList.map((a:any, index:any)=>{
+      if (product.id === a.id){
+        this.cartItemList.splice(index, 1);
+      }
+    })
+    this.productList.next(this.cartItemList);
+  }
+
+  removeAllCart(){
+    this.cartItemList = [];
     this.productList.next(this.cartItemList);
   }
 }
